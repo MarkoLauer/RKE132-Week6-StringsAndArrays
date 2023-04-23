93e7496004020e657f01da581f8d2fbde806423c
MarkoLauer <marko.lauer@tktk.ee>
2023-04-23 21:33:52 +0300

    add week6 Strings and Arrays

---
+++ RandomElement/Program.cs
@@ -0,0 +1,14 @@
+﻿string[] snacks = { "sushi", "pizza", "burger", "chicken wings", "asian takeaways" };
+
+Random rnd = new Random();
+
+int randomIndex = rnd.Next(0, snacks.Length);
+
+Console.WriteLine($"Tonoght we are gonig to eat {snacks[randomIndex]}");
+
+string[] colors = { "red", "green", "blue", "pink", "yellow" };
+Random rnd = new Random();
+string randomColor = colors[rnd.Next(0, colors.Length)];
+
+Console.WriteLine($"You should wear {randomColor} today.");
+
